49ed8d1e7077d424cbf503538668821403920d07
Suraj <suraj.k.sahu2002@gmail.com>
2022-10-30 00:03:26 +0530

    added react

--- Frontend/Javascript/index.js
+++ Frontend/Javascript/index.js
@@ -1,1 +1,12 @@
-ReactDom.render(<h1>Hello,everyone</h1>,document.getElementsById("root"));
+function mainContent()
+{
+    return(
+            <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia in totam soluta iste? Repellendus quis id, nemo iure aspernatur quaerat ullam nihil quisquam obcaecati minus natus doloribus molestias sunt corporis?
+            </p>
+    )
+}
+
+ReactDOM.render(
+    <mainContent />,
+document.getElementById("root"))
